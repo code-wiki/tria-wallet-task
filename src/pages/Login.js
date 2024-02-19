e3bf1b0d0a0a6d835109737254bbabf3372abfea
code-wiki <aditya.gautam003@gmail.com>
2024-02-19 11:14:40 +0530

    added netlify url support

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -28,7 +28,7 @@ const LoginPage = (props) => {
     }
     return (
         <>
-            <div className="bg-black h-screen ">
+            <div className="bg-black h-screen">
                 <div className="fixed top-0 left-0 z-[89] h-0 w-0">
                     <div className="rounded-[20] overflow-hidden fixed top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]">
                         <div className="w-[448px] rounded-[20px] shadow-[rgba(0, 0, 0, 0.15) 0px 4px 12px] flex-col justify-between inline-flex transform scale-[0.7] p-[16px] h-[840px] bg-[#101010]">
